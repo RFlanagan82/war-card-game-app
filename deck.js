cc7deb8cc85d249d96c2720923c86f5ea4105677
Ryan Flanagan <flanagan.ryan@gmail.com>
2020-11-30 14:41:15 -0500

    added some styling to the card counter decks

--- deck.js
+++ deck.js
@@ -16,9 +16,23 @@ const VALUES = [
 ];
 
 export default class Deck {
-    //set cards by default to a fresh deck - so we pass in a set # of cards then we'll have a new deck of every single card
+  //set cards by default to a fresh deck - so we pass in a set # of cards then we'll have a new deck of every single card
   constructor(cards = freshDeck()) {
     this.cards = cards;
+  }
+
+  get numberOfCards() {
+    return this.cards.length;
+  }
+
+  //loop through all the cards and swap with another card
+  shuffle() {
+    for (let i = this.numberOfCards - 1; i > 0; i--) {
+       const newIndex = Math.floor(Math.random() * (i + 1))
+       const oldValue = this.cards[newIndex]
+       this.cards[newIndex] = this.cards[i];
+       this.cards[i] = oldValue; 
+    }
   }
 }
 
@@ -30,6 +44,7 @@ class Card {
 }
 
 // this will create a brand new deck of 52 cards - one for each suit and value combination
+//use .flatMap funtion to put all the cards into one array of all 52 cards, not 4 arrays of 13 cards based on their suits.
 function freshDeck() {
   return SUITS.flatMap((suit) => {
     return VALUES.map((value) => {
